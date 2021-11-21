52a918ecd41122b19758517c2bfb7b8cc87a4d25
Black <vothanhlam1793@gmail.com>
2021-11-22 04:48:49 +0700

    ver-1.0.0

--- main.js
+++ main.js
@@ -187,6 +187,6 @@ app.get('*', function(req, res){
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, "192.168.1.43",() => {
+app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}.`);
 });
